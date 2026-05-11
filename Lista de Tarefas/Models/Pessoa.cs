1fafa0a23f4747fa75f348abda1dba12ab036d37
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-05-11 10:15:34 -0300

    atualizar

--- Lista de Tarefas/Models/Pessoa.cs
+++ Lista de Tarefas/Models/Pessoa.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
-        public int Senha { get; set; }
+        public string Senha { get; set; }
     }
 }

9d18f6fa253d66f71102d5346bfdb37b6620ce1e
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-05-19 11:14:17 -0300

    atualizar

--- js/listadetarefas.js
+++ js/listadetarefas.js
@@ -1,65 +1,101 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-   
-    fetch('https://localhost:7095/tarefa/reservas/',{  credentials:"include"
-})
 
-     
+    fetch('https://localhost:7095/tarefa/reservas/', {
+        credentials: "include"
+    })
+
     .then(response => response.json())
-        .then(data => {
-            console.log (data);
 
-           var resposta = document.getElementById("listadetarefas");
-           resposta.innerHTML ="<h4>Segue Lista de Tarefas</h4> ";      
+    .then(data => {
 
-         for (let i = 0; i < data.length; i++) {
+        console.log(data);
 
-    resposta.innerHTML += `
-        <ul>
-            <li>
-                Descrição: ${data[i].tarefa}
-                <button onclick="apagarTarefa(${data[i].id})">
-                    apagar
-                </button>
-                <select name="" id="status">
+        var resposta = document.getElementById("listadetarefas");
+
+        resposta.innerHTML = "<h4>Segue Lista de Tarefas</h4>";
+
+        for (let i = 0; i < data.length; i++) {
+
+            resposta.innerHTML += `
+            
+            <div>
+
+                Tarefa:
+                
+                <input 
+                    type="text" 
+                    id="descricao${data[i].id}"
+                    value="${data[i].tarefa}"
+                >
+
+                <select>
                     <option disabled selected>Status</option>
                     <option>Afazer</option>
                     <option>Feito</option>
                 </select>
-            </li>
-        </ul>
-    `;
-}
-     
-        })
 
+                <button onclick="apagarTarefa(${data[i].id})">
+                    apagar
+                </button>
+
+                <button onclick="editarReserva(${data[i].id})">
+                    editar
+                </button>
+
+            </div>
+            
+            `;
+        }
+
+        var nome = document.getElementById("nome");
+
+        nome.innerHTML = "Olá " + data[0].pessoa+ " <button id='Logout'>Sair</button>";
+
+    });
 
 });
 
-
-
 function apagarTarefa(id) {
 
-    fetch(`https://localhost:7095/tarefa/Deletar/${id}`, {
+    fetch('https://localhost:7095/tarefa/' + id, {
+
         method: "DELETE",
         credentials: "include"
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Erro ao apagar tarefa");
-        }
-        return response.text(); 
-    })
-    .then(() => {
-        alert("Tarefa apagada com sucesso!");
 
-      
-        carregarTarefas();
     })
-    .catch(error => {
-        console.error(error);
-        alert("Erro ao apagar tarefa");
-    });
+
+    .then();
+
 }
 
+function editarReserva(idTarefa) {
 
+    fetch('https://localhost:7095/tarefa/' + idTarefa, {
+
+        method: 'PUT',
+
+        credentials: 'include',
+
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+
+            descricao: document.getElementById("descricao" + idTarefa).value,
+
+        }),
+
+    })
+
+    .then(response => response.text())
+
+    .then(data => {
+
+        console.log(data);
+
+        location.reload();
+
+    });
+
+}
